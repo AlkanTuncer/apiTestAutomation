571a3dd20a850ece83e05dbd90ddc437afb0bcfa
AlkanTuncer <alkantuncer53@hotmail.com>
2022-09-20 21:52:27 +0300

    SprinGular _ Token usage

--- src/test/java/apiTests/_02_Token.java
+++ src/test/java/apiTests/_02_Token.java
@@ -1,10 +1,6 @@
 package apiTests;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
